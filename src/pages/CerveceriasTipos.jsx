e18a2226fa21299776059960c58623990e570480
franciscoruiz85 <francisco.ruizs2015@gmail.com>
2025-05-08 13:04:51 -0400

    [modify]: Paises.jsx and CerveceriasTipos.jsx

--- src/pages/CerveceriasTipos.jsx
+++ src/pages/CerveceriasTipos.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React from "react"
+import {
+  Container,
+  Typography
+} from "@mui/material"
+
+const tipos = await import("../tipos.json").then((module) => module.default)
 
 const CerveceriasTipos = () => {
   return (
-    <div>CerveceriasTipos</div>
+    <Container>
+      <Typography
+        variant="h4"
+        sx={{
+          textAlign: "center",
+          marginBottom: "30px",
+          fontWeight: "bold"
+        }}
+      >
+        Tipos de Cervecerias
+      </Typography>
+      {tipos.map((item) => (
+        <div 
+          key={ item.key }
+          style={{
+            border: "1px solid #ccc",
+            borderRadius: "8px",
+            padding: "20px",
+            marginBottom: "20px"
+          }}
+        >
+          <Typography
+            variant="h5"
+            sx={{ fontWeight: 'bold' }}
+          >
+            { item.name }:
+          </Typography>
+          <Typography variant="body1">
+            { item.description }
+          </Typography>
+        </div>
+      ))}
+    </Container>
   )
 }
 
--- src/pages/Paises.jsx
+++ src/pages/Paises.jsx
@@ -1,8 +1,62 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import {
+  Container,
+  Grid,
+  Card,
+  CardActionArea,
+  CardContent,
+  Typography
+} from '@mui/material'
+import Cerveceria from '../assets/cerveceria.jpg'
+
+const paises = await import('../paises.json').then(module => module.default)
 
 const Paises = () => {
   return (
-    <div>Paises</div>
+    <Container>
+      <Grid
+        container
+        sx={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginBottom: '20px'
+        }}
+      >
+        <img id='cerveceria' src={ Cerveceria } />
+      </Grid>
+      <Grid
+        container
+        rowSpacing={ 2 }
+        columnSpacing={ 2 }
+        sx={{
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        { paises.map((item) => (
+          <Grid key={ item.key }>
+            <Card sx={{ minWidth: 250, maxWidth: 345 }}>
+              <CardActionArea
+                component={ Link }
+                to={ `/paises/${item.name.replace(/\s+/g, '_')}` }
+                style={{ textDecoration: 'none', color: 'inherit' }}
+                state={{ country: item }}
+              >
+                <CardContent>
+                  <Typography
+                    gutterBottom
+                    variant="h5"
+                  >
+                    { item.name }
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        )) }
+      </Grid>
+    </Container>
   )
 }
 
